4682171a402f76622b82a3af59b6675382f4a775
VanessaRV <vanessa.rascon@hotmail.com>
2021-03-15 16:16:34 +0100

    Updated vignettes and added documentation for printe.

--- R/printer.R
+++ R/printer.R
@@ -1,13 +1,17 @@
-#' Title
+#' Silly printer function. Test #2
 #'
-#' @param r
-#' @param x
-#' @param y
+#' @param r what you want in second column
+#' @param x what you want in first column
 #'
-#' @return
+#' @return a tibble
 #' @export
 #'
+#' @importFrom tibble data_frame
+#' @importFrom utils head
 #' @examples
-print = function(r, x, y){
-  print(paste0("x= ",x))
+#' printer(x = rnorm(5), r= rnorm(5))
+printer = function(r, x){
+  x = data_frame(x = x, r = r)
+  print(head(x))
+  return(x)
 }
